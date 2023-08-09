eb77a69d637f915d650af779dd1f201f40e8db44
Ibnu <alyazzar14@gmail.com>
2023-08-09 14:51:37 +0700

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,16 +35,48 @@ export class AppComponent {
   }
 
   increment() {
-      setInterval(() => {
-        this.num++
-        if (this.num > 5) {
-          this.numStyles.color = 'red'
-        }
-      }, 1000)
+    const interval = setInterval(() => {
+      this.num++
+      if (this.num > 5) {
+        this.numStyles.color = 'red'
+      }
+
+      if (this.num >= 10) {
+        clearInterval(interval)
+        this.num = 10
+      }
+    }, 1000)
+
 
     // if (this.num === 10) {
     //   clearInterval()
     // }
   }
+
+
+  message: string = "<h1>Mouse Hovew me mew ≽^•⩊•^≼</h1>"
+
+  mOver(): void {
+    this.message = '<h1>Thanks fow hovew me mew ≽^•⩊•^≼</h1>'
+  }
+
+  mOut(): void {
+    this.message = '<h1>Hovew me again mew ≽^•⩊•^≼</h1>'
+  }
+
+  name: string = ''
+  onKeyPress(ev: any): void {
+    console.log(ev)
+    console.log("---")
+    console.log(ev.target.value)
+    console.log("---")
+    console.log(ev.data)
+    console.log("---")
+    this.name = ev.target.value
+  }
+
+  tes: any[] = []
+  isActive: boolean = false
+
 
 }
