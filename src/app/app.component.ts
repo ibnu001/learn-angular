bbe2c3cdb3a6f11d124ae770e01b3b9459efea83
Ibnu <alyazzar14@gmail.com>
2023-08-09 09:23:18 +0700

    property-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+
+  imgSrc: string = 'assets/images/ari.jpg'
+  imgAlt: string = 'ARIIII'
+
+  fullname: string = 'My name is <strong> Ibnu </strong>'
+  color: string = 'blue'
+
+  styles = {
+    color: '#fff',
+    fontSize: '2rem',
+    fontWeight: '600',
+  }
+
+  isDisabled: boolean = false
+
+  num: number = 0
+
+  numStyles = {
+    color: 'blue'
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isDisabled = !this.isDisabled
+    }, 2000)
+  }
+
+  increment() {
+      setInterval(() => {
+        this.num++
+        if (this.num > 5) {
+          this.numStyles.color = 'red'
+        }
+      }, 1000)
+
+    // if (this.num === 10) {
+    //   clearInterval()
+    // }
+  }
+
 }
